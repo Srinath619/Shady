ca5e4dfc603255b97c6a7e30ff39c85783cbd110
Srinath619 <36729647+Srinath619@users.noreply.github.com>
2018-02-23 14:11:38 +0530

    Create baseheioftri.c

---
+++ baseheioftri.c
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int c,h,a;
+  clrscr();
+  cout<<"enter the base and height of a triangle";
+  cin>>c>>h
+  a=(c*h)/2
+  cout<<"area is"<<a;
+  getch();
+}
